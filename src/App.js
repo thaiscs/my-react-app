e9b73db103ba5ea4b0895853d2f4866f9a93aa5e
thaiscs <thais.advj@gmail.com>
2019-11-18 11:27:06 +0100

    first change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Decompose an interfae into Components <code>src/App.js</code> and save to reload!
+          Decompose an interface into Components <code>and save to reload!</code> 
         </p>
         <a
           className="App-link"
